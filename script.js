dc9ee4d23cdde9dfbe1fab92f928969c5bb270a0
Mohd Shadab <shadab_ibrahim@outlook.com>
2023-06-27 20:48:18 +0530

    submitting code-Tue Jun 27 2023 20:48:18 GMT+0530 (India Standard Time)

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+let link = document.createElement("a")
+link.innerHTML = "https://www.youtube.com/watch?v=8sLS2knUa6Y&pp=ygUEc29uZw%3D%3D";
+
+let divUrl = document.querySelector("#url");
+divUrl.appendChild(link);
